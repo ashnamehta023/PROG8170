8db624fea05e73511be7ba06eb037ffadede1135
ashna <amehta0082@conestogac.on.ca>
2020-02-03 19:12:35 -0500

    Initial

---
+++ Prog8170Assignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prog8170Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
